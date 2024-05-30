576e57d418fb96af613373baabd45145a3611798
Jocki Hendry <jocki.hendry@gmail.com>
2024-05-31 03:18:31 +0700

    Allow reading the blog while offline

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import starlightBlog from 'starlight-blog';
 import tailwind from "@astrojs/tailwind";
 import rehypeMermaid from 'rehype-mermaid';
 import starlightImageZoom from 'starlight-image-zoom';
+import serviceWorker from 'astrojs-service-worker';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -31,8 +33,23 @@ export default defineConfig({
     }
   }), mdx(), tailwind({
     applyBaseStyles: false,
+  }), serviceWorker({
+    workbox: {      
+      manifestTransforms: [
+        (manifest) => {          
+          manifest.forEach(m => m.url = (m.url.endsWith('/index.html')) ? m.url.substring(0, m.url.length - 11) : m.url)
+          return { manifest }                    
+        }
+      ],
+      ignoreURLParametersMatching: [/^utm_/, /^fbclid$/, /ts$/],
+      runtimeCaching: [{
+        urlPattern: () => true,
+        handler: "StaleWhileRevalidate",
+      }],        
+    }
   })],
   markdown: {
     rehypePlugins: [[rehypeMermaid, {strategy: "img-svg"}]],
-  }
+  },
+  trailingSlash: 'never',
 });
