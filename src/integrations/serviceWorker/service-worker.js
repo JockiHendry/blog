1d64f8f10c2342f7d38535fd66736474a0c45c9e
Jocki Hendry <jocki.hendry@gmail.com>
2024-06-14 17:33:41 +0700

    Remove StateWhileRevalidate strategy

--- src/integrations/serviceWorker/service-worker.js
+++ src/integrations/serviceWorker/service-worker.js
@@ -1,13 +1,13 @@
 import { clientsClaim } from "workbox-core";
 import { precacheAndRoute, cleanupOutdatedCaches } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
-import { NetworkFirst, StaleWhileRevalidate} from "workbox-strategies";
+import { NetworkFirst } from "workbox-strategies";
 
 self.skipWaiting();
 clientsClaim();
 registerRoute(/.*\/blog$/, new NetworkFirst(), 'GET');
-registerRoute(() => true, new StaleWhileRevalidate(), 'GET');
 precacheAndRoute(self.__WB_MANIFEST, {
-  "ignoreURLParametersMatching": [/^utm_/, /^fbclid$/, /ts$/]
+  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/, /ts$/],  
+  urlManipulation: ({url}) => [new URL(url.href + '/index.html')],  
 });
 cleanupOutdatedCaches();
